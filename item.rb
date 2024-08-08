7841f5780ae0df4eb0e0ffd7164d94a87481e3b7
Elton Okada <eltonokada@gmail.com>
2024-08-08 16:19:20 -0300

    Initial Objects structure

---
+++ item.rb
@@ -0,0 +1,9 @@
+class Item
+  attr_accessor :title, :price
+
+  def initialize(title, price)
+    @title = title
+    @price = price
+  end
+
+end
---
+++ pricing_rule.rb
@@ -0,0 +1,7 @@
+class PricingRule  
+  def initialize(item, rule_type)
+    @item = item
+    @rule_type = rule_type
+  end
+
+end
